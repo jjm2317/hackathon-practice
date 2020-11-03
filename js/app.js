f6b7f2d8d7eb659a03896ce6f2742b6c944aaa86
young seo <gg8682@naver.com>
2020-11-03 16:08:56 +0900

    fix : dlskd

---
+++ js/app.js
@@ -0,0 +1,16 @@
+// State
+let todos = [];
+let todosState = [];
+
+// DOMs
+const $todos = document.querySelector('.todos');
+const $inputTodo = document.querySelector('.input-todo');
+const $completeAll = document.querySelector('.complete-all');
+const $clearCompleted = document.querySelector('.clear-completed');
+const $allCheckbox = document.querySelector('.complete-all > .checkbox');
+const $completedTodos = document.querySelector('.completed-todos');
+const $itemsLeft = document.querySelector('.active-todos');
+const $all = document.querySelector('#all');
+const $active = document.querySelector('#active');
+const $completed = document.querySelector('#completed');
+const $nav = document.querySelector('nav');
